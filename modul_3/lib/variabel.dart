52db6d2fcba30f52b4ed85711a187ccee1e60092
ricofasha <ricofasha1410@gmail.com>
2025-03-09 22:43:09 +0700

    up

---
+++ modul_3/lib/variabel.dart
@@ -0,0 +1,16 @@
+void main() {
+  int myInt = 22;
+  double myDouble = 20.8;
+  String myString = "Hello, rico!";
+  bool myBool = false;
+  List<String> myList = ['Bandung', 'Surabaya', 'Jakarta'];
+  Map<String, int> myMap = {'Alpukat': 1, 'Durian': 2};
+
+  // Print variable sesuai yang di deklarasikan
+  print(myInt);
+  print(myDouble);
+  print(myString);
+  print(myBool);
+  print(myList);
+  print(myMap);
+}
